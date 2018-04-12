bca76340c0f42c6ab183c2b5f820100d09fe4cdf
Omar Desogus <omardesogus9@gmail.com>
2018-04-12 16:01:32 +0200

    [refactor] update project links

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,9 +1,11 @@
 export class Project {
 
     name: string;
+    link: string;
 
     constructor(project: Project) {
         this.name = project.name;
+        this.link = project.link;
     }
 
 }
