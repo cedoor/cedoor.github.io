74d826f979e2a0cb3cd0e2a5fecbdd35a1ec43bc
cedoor <me@cedoor.dev>
2024-11-16 19:20:21 +0700

    docs: update about and now pages

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/gallery', title: 'Gallery' },
-  { href: '/now', title: 'Now' },
+  // { href: '/now', title: 'Now' },
   //{ href: '/tags', title: 'Tags' },
   { href: '/', title: 'About' },
 ]
